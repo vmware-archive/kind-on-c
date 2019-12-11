97330c7cb5ad7ec5131a2e51c7962fa836b48e60
Hannes Hörl <hhorl@pivotal.io>
2019-12-11 22:37:14 +0000

    [ci skip] Check resources when setting the pipeline
    Example:
    After a new kubernetes version has been released, we change the pipeline
    to use a different branch for the resources `k8s-git-current{,-minus-1,minus-2}`.
    Because those resource are not checked that often,we now might end up
    with with a resource version (git revision) that is not on the
    resource's branch after we changed that.
    As a workaround we ask concourse to check those resources right after we
    set the pipeline, which should give us a valid resource version.

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -10,11 +10,37 @@ readonly CI_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 readonly TMP_DIR="$( mktemp -d )"
 trap 'rm -rf -- "$TMP_DIR"' EXIT
 
+flySync() {
+  fly sync "$@"
+}
+
+checkResAsync() {
+  local resToCheck=(
+    'k8s-git-master' 'k8s-git-current' 'k8s-git-current-minus-1' 'k8s-git-current-minus-2'
+  )
+  local pids=()
+
+  for r in "${resToCheck[@]}"
+  do
+    fly check-resource -r "${PIPELINE_NAME}/${r}" --async "$@" &
+    pids+=( $! )
+  done
+
+  waitForAll "${pids[@]}"
+}
+
+waitForAll() {
+  for pid in "$@"
+  do
+    wait "$pid"
+  done
+}
+
 main() {
   local varsFile="${TMP_DIR}/vars.yaml"
   local pipelineFile="${CI_DIR}/pipeline.yaml"
 
-  fly sync "$@"
+  flySync "$@" >/dev/null
 
   lpass show --notes "$LPASS_PATH" > "$varsFile"
 
@@ -23,6 +49,8 @@ main() {
     --pipeline="${PIPELINE_NAME}" \
     --config="${pipelineFile}" \
     "$@"
+
+  checkResAsync "$@" >/dev/null
 }
 
 main "$@"
